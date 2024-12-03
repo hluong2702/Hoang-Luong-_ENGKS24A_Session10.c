0a8893dcfecb0c10cc22844868d647d5e6145236
hluong2702 <hoangluong2702@gmail.com>
2024-12-03 14:26:08 +0700

    ss10

--- Session10ex7.c
+++ Session10ex7.c
@@ -1,42 +1,36 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int m) {
-   for(int i = 0;i<m;i++){
-        for(int j = 0;j<m-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp;
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-        }
-    }
-}
-
 int main() {
     int n, m;
-
-    printf("Moi ban nhap vao so hang ");
-    scanf("%d",&m);
-
-    printf("Moi ban nhap vao so cot ");
-    scanf("%d",&n);
-
-    int arr[m][n];
-    for(int i = 0;i<m;i++){
-        for(int j =0;j<n;j++){
-            printf(" arr[%d][%d] = ",i,j);
-            scanf("%d",&arr[i][j]);
+    printf("Nhap so dong: ");
+    scanf("%d", &n);
+    printf("Nhap so cot: ");
+    scanf("%d", &m);
+    int arr[n][m];
+    
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &arr[i][j]);
         }
+        printf("-----\n");
     }
 
     for (int i = 0; i < n; i++) {
-        bubbleSort(arr[i], m);
+        for (int j = 0; j < m - 1; j++) {
+            for (int k = 0; k < m - j - 1; k++) {
+                if (arr[i][k] > arr[i][k + 1]) {
+                    int temp = arr[i][k];
+                    arr[i][k] = arr[i][k + 1];
+                    arr[i][k + 1] = temp;
+                }
+            }
+        }
     }
 
-    printf("\nMa tran sau khi sap xep:\n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+    printf("Mang sau khi sap xep theo tung dong:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             printf("%d ", arr[i][j]);
         }
         printf("\n");
